9e4c1260888744a8b30a5a09a14f515cddb5d4b4
DMK980 <derrick.mutuma@gmail.com>
2025-02-08 15:04:55 +0300

    deploying now

--- api/app.js
+++ api/app.js
@@ -13,9 +13,24 @@ app.get('/',(req,res)=>{
     res.json({message:"Hello World!"})
 })
 app.post('/shortening',async (req,res)=>{
+
+    function inputValidation(url){
+        /*
+            Function makes sure the url 
+            is in good format for the API
+        */
+       if(!url.includes('https') || !url.includes('http')){
+        let validUrl = `https://${url}`
+        return validUrl
+       } else{
+        return url
+       }
+    }
+
     try {
+        const validUrl = inputValidation(req.body.url)
         const data = await axios.post("https://cleanuri.com/api/v1/shorten",{
-            url:req.body.url
+            url:validUrl
         })
         res.json({response:data.data.result_url}) 
     } catch (error) {
